f4aba72d632fb85c61797586648d88e93d11d1c0
prasanthk004 <prasanthkishore004@gmail.com>
2022-04-16 21:42:47 +0530

    Added Random Numbers

--- ArrayDimensions.java
+++ ArrayDimensions.java
@@ -15,11 +15,13 @@ public class ArrayDimensions {
 	private void TwoDArray() {
 		// TODO Auto-generated method stub
 		
-		int a[][]=new int[2][2];
+		int a[][]=new int[2][5];
 		Random r = new Random();   
 
 		
-		int i , j ;
+		int i , j,tot=0 ;
+		
+		String[] sub= {"Tamil","English","Maths","Science","S.Science"};
 		
 	/*	System.out.println(" a len : " +a.length);
 		System.out.println(" a[i] len :"+a[0].length);
@@ -29,15 +31,17 @@ public class ArrayDimensions {
 		
 		for(i=0;i<a.length;i++)
 		{
+			System.out.printf("\n\n Marks of Student No  : %d \n",i+1);
 			for(j=0;j<a[i].length;j++)
 			{
-				a[i][j]=r.nextInt(9)+1;
-				System.out.printf(" %3d ",a[i][j]);
+				a[i][j]=r.nextInt(99)+1;
+				System.out.printf("\n %-10s      :  %3d ",sub[j],a[i][j]);
+				tot+=a[i][j];
 			}
-			System.out.println();
-			
-		}
-		
+			  System.out.printf("\n----------------------------------");
+			   System.out.printf("\n %-10s      :  %3d ","Total",tot);
+			   tot=0;
+		}		
 		
 		
 	}
