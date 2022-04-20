dbf95dcf137f924833365f503966c41c7fd0271c
prasanthk004 <prasanthkishore004@gmail.com>
2022-04-20 21:21:55 +0530

    Initial file creation & Added String Analysis freature

---
+++ StringOperations.java
@@ -0,0 +1,96 @@
+package Java.basics;
+
+public class StringOperations {
+	
+	String x="k p 8@";
+	
+	public static void main(String[] args) {
+		StringOperations s = new StringOperations();
+		s.StringAnalysis();
+
+	}
+
+	private void StringAnalysis() {
+		// TODO Auto-generated method stub
+		
+		int i ;
+		int ac=0,sc=0,nc=0,Cc=0,Smc=0,oc=0,wc=0;
+		char c ;
+		int chrc=0;
+		int wflag=0;
+		
+		for(i=0;i<x.length();i++)
+		{
+			c=x.charAt(i);
+			
+			//Finding No.Of Characters & No of Digits 
+			if((c>='A'&& c<='Z')||(c>='a' && c<='z'))
+			{
+				ac++;
+			 
+				
+				if(c>='A' && c<='Z')
+				{
+					Cc++;
+				}
+				else if(c>='a'&&c<='z')
+				{
+					Smc++;
+				}
+				
+			}
+			else if(c>='0'&&c<='9')
+			{
+				nc++;
+			 
+			}			
+			else
+			{
+				oc++;
+			}
+			
+			
+			// Finding No of Words
+			
+			if(c==' '||c=='\t'||c=='\n')
+			{
+				sc++;
+				if(wflag==1)
+				{
+					wc++;
+					wflag=0;
+					
+				}
+			}
+			else {
+				wflag=1 ;
+			}
+			
+			chrc++;
+		}
+		if(x.charAt(x.length()-1)!=' ') wc++; // IF last character is not space  adding word count mannually "
+	 System.out.println("  STRING ANALYSIS ");
+		System.out.println("  -------------- ");
+		System.out.printf("\n\n");
+		System.out.println(" Total Characters  : "+chrc);
+		System.out.println(" Alphabets         : "+ac);
+		System.out.println(" Capitals Letters  : "+Cc);
+		System.out.println(" Small    Letters  : "+Smc);
+		System.out.println(" Numbers           : "+nc);
+		System.out.println(" Spaces            : "+sc);
+	   	System.out.println(" Symbols           : "+oc);
+	   	System.out.println(" Words             : "+wc);
+
+		
+	}
+
+	 
+	 
+	}
+	
+	
+	
+	
+	
+
+ 
