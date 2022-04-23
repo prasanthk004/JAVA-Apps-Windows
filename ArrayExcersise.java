3562409307ea7c90e92a4ec88c0c54c7fd20f44e
prasanthk004 <prasanthkishore004@gmail.com>
2022-04-23 10:24:14 +0530

    Added New file and Initial Commit

---
+++ ArrayExcersise.java
@@ -0,0 +1,22 @@
+package Java.basics;
+
+public class ArrayExcersise {
+	
+	int a[]= {1,2,3,4,5,6,7,8,9,10};
+	
+	public static void main(String[] args) {
+		ArrayExcersise ar= new ArrayExcersise();
+		ar.PrintArrayMaxMin();
+	}
+
+	private void PrintArrayMaxMin() {
+		// TODO Auto-generated method stub
+		int i;
+		
+		for(i=0;i<a.length/2;i++)
+		{
+			System.out.printf("%3d %3d",a[i],a[a.length-i-1]);
+		}
+	}
+
+}
